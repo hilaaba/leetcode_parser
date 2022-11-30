369f63955f60747aad7b85704bd66629a946123e
Ivanov Viacheslav <viachmerc@gmail.com>
2022-11-30 21:56:55 +0300

    first working version

--- main.py
+++ main.py
@@ -1,60 +1,39 @@
-import csv
-import re
-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import requests
-from bs4 import BeautifulSoup
-from requests_html import HTMLSession
+import csv
 
-URL = 'https://leetcode.com/problemset/algorithms/?page=42/'
+URL = 'https://leetcode.com/graphql'
 FIELD_NAMES = ('id', 'title', 'acceptance', 'difficulty')
 DEFAULT_FILENAME = 'leetcode_problems.csv'
 
+data = {
+    "variables": {
+        "categorySlug": "algorithms",
+        "skip": 0,
+        "limit": 10,
+        "filters": {},
+    },
+    "query": "query problemsetQuestionList($categorySlug: String, $limit: Int, $skip: Int, $filters: QuestionListFilterInput) {problemsetQuestionList: questionList(categorySlug: $categorySlug limit: $limit skip: $skip filters: $filters) {total: totalNum questions: data {acRate difficulty freqBar frontendQuestionId: questionFrontendId isFavor paidOnly: isPaidOnly status title titleSlug topicTags {name id slug} hasSolution hasVideoSolution}}}",
+}
 
-def parse_rows(table: BeautifulSoup) -> list:
-    rows = []
-    for row in table.contents:
-        tmp = []
-        for cell in row.contents:
-            if not cell.text:
-                continue
-            elif re.match(r'\d+\. \w', cell.text):
-                id, title = cell.text.split('. ')
-                tmp.append(int(id))
-                tmp.append(title)
-            elif re.match(r'\d+\.\d%', cell.text):
-                tmp.append(float(cell.text[:-1]))
-            else:
-                tmp.append(cell.text.lower())
-        rows.append(tmp)
-    return rows
+response = requests.post(URL, json=data).json()
 
+problems_list = response.get('data').get('problemsetQuestionList').get('questions')
 
-def main():
-    # driver = webdriver.Chrome()
-    # driver.implicitly_wait(15)
-    # driver.get(URL)
-    # WebDriverWait(driver, 15)3
-    session = HTMLSession()
-    response = session.get(url=URL)
-    response.html.render()
-    print(response.html.raw_html)
+result = []
+for problem in problems_list:
+    id = problem.get('frontendQuestionId')
+    title = problem.get('title')
+    acceptance = round(problem.get('acRate'), 1)
+    difficulty = problem.get('difficulty').lower()
+    result.append((id, title, acceptance, difficulty))
 
-    # response = requests.get(URL, timeout=(5, 15), stream=True)
-    # soup = BeautifulSoup(response.html.raw_html, features='html.parser')
-    # table = soup.find(role='rowgroup')
-    # rows = parse_rows(table)
-    # print(rows)
+with open(DEFAULT_FILENAME, 'w') as file:
+    writer = csv.writer(file)
+    writer.writerow(FIELD_NAMES)
+    writer.writerows(result)
 
 
-    # with open(DEFAULT_FILENAME, 'w') as file:
-    #     writer = csv.writer(file)
-    #     writer.writerow(FIELD_NAMES)
-    #     writer.writerows(rows)
 
 
 if __name__ == '__main__':
-    main()
+    pass
